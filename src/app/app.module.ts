52bc6dada170033cb41c73d08f6b05b8d356b40d
Kamalka Fernando <rathnaweerakamalka@gmail.com>
2020-07-15 09:19:53 +0530

    initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,47 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { NgxPageScrollModule } from 'ngx-page-scroll';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { DiseasesComponent } from './diseases/diseases.component';
+import { CustomerSectionComponent } from './customer-section/customer-section.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { FooterComponent } from './footer/footer.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    AboutusComponent,
+    DiseasesComponent,
+    CustomerSectionComponent,
+    ContactUsComponent,
+    FooterComponent,
+    SignInComponent,
+    SignUpComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgxPageScrollModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatSelectModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
