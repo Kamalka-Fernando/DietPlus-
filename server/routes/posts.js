89218f3542e0767043c6abe16842625458747e0f
Kamalka Fernando <rathnaweerakamalka@gmail.com>
2020-09-28 21:42:00 +0530

    Node connecting...attempt 1

---
+++ server/routes/posts.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+
+const axios = require('axios');
+
+const PostAPI = 'https;//jsonplaceholder.typicode.com';
+//Get post
+router.get('/',(req, res)=>{
+  axios.get(`${PostAPI}/posts`).then(posts=>{
+    console.log(posts.data);
+  })
+});
+
+module.exports = router;
